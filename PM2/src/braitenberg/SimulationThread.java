32250b55770035557e869b61db51d566c22a4629
Bobbel0815 <bobbele.one@gmx.de>
2016-12-08 17:47:07 +0100

    Update SimulationThread.java

--- PM2/src/braitenberg/SimulationThread.java
+++ PM2/src/braitenberg/SimulationThread.java
@@ -1,17 +1,24 @@
 package braitenberg;
 
 import braitenberg.braitenbergvehikel.BVSimulation;
-
+/**
+ * Repärsentiert einen Thread, welcher 200ms schläft und mit einem FLag beendet werden kann.
+ */
 public class SimulationThread extends Thread {
 
 	private BVSimulation simulation;
+/**
+ * Variable zum verlassen der while-Schleife
+ */
 	private boolean alive;
 
 	public SimulationThread(BVSimulation simulation) {
 		this.simulation = simulation;
 		alive = true;
 	}
-	
+	/**
+	 * Flag zum Beenden des Threads.
+	 */
 	void setDead(){
 		alive = false;
 	}
